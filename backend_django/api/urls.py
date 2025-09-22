209f3fe5023d64d04fc2c42052d0a1d05890228e
Amanbundela75 <aman75bundela@gmail.com>
2025-09-22 14:42:54 +0530

    Update API URLs to include auth sync and admin routes

--- backend_django/api/urls.py
+++ backend_django/api/urls.py
@@ -1,1 +1,10 @@
-# API app URLs
+from django.urls import path
+from .views import RegisterOrLoginView
+from django.contrib import admin
+from django.urls import path, include # 'include' ko import karein
+
+urlpatterns = [
+    path('auth/sync/', RegisterOrLoginView.as_view(), name='auth_sync'),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')), # Hamare API app ke URLs
+]
