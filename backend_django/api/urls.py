5a294ea691271fa8f868185e01f77c84f537eb42
Amanbundela75 <aman75bundela@gmail.com>
2025-09-30 12:44:01 +0530

    Add PostListCreateView to API routes for handling post listing and creation

--- backend_django/api/urls.py
+++ backend_django/api/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from .views import RegisterOrLoginView
+from .views import RegisterOrLoginView, PostListCreateView
 from django.contrib import admin
 from django.urls import path, include # 'include' ko import karein
 
 urlpatterns = [
     path('auth/sync/', RegisterOrLoginView.as_view(), name='auth_sync'),
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')), # Hamare API app ke URLs
+    path('api/v1/', include('api.urls')),
+    path('posts/', PostListCreateView.as_view(), name='post_list_create'),# Hamare API app ke URLs
 ]
