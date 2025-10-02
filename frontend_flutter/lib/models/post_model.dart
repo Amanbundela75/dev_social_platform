8bc406a8f5407a252feb4a63c7ed110b99fae6ab
Amanbundela75 <aman75bundela@gmail.com>
2025-10-02 10:01:55 +0530

    Add Post model with JSON deserialization factory constructor

--- frontend_flutter/lib/models/post_model.dart
+++ frontend_flutter/lib/models/post_model.dart
@@ -1,1 +1,23 @@
-// Post model
+class Post {
+  final int id;
+  final String authorUsername;
+  final String content;
+  final DateTime createdAt;
+
+  Post({
+    required this.id,
+    required this.authorUsername,
+    required this.content,
+    required this.createdAt,
+  });
+
+  // JSON se Post object banane ke liye factory constructor
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      authorUsername: json['author_username'],
+      content: json['content'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+}
