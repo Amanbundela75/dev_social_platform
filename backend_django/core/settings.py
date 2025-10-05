3fe81ceea3a353f32c4cdb1be2a879f5245bb4da
Amanbundela75 <aman75bundela@gmail.com>
2025-10-05 22:41:49 +0530

    Configure Django project with `TEMPLATES`, `MIDDLEWARE`, and PostgreSQL `DATABASES` settings.

--- backend_django/core/settings.py
+++ backend_django/core/settings.py
@@ -12,3 +12,40 @@ INSTALLED_APPS = [
     # Local apps
     'api',
 ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev_social_db',      # <-- Yahan apne database ka naam daalo
+        'USER': 'postgres',           # <-- Aapka PostgreSQL username
+        'PASSWORD': 'your_password',  # <-- Aapka PostgreSQL password
+        'HOST': 'localhost',          # '127.0.0.1' bhi daal sakte hain
+        'PORT': '5432',               # Default port yahi hota hai
+    }
+}
